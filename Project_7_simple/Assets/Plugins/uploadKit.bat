3d78b1936e6e214fd4301659526bbe9048c7e099
omcgrath2014 <>
2020-10-10 01:53:09 -0700

    Add coloration

--- Project_7_simple/Assets/Plugins/uploadKit.bat
+++ Project_7_simple/Assets/Plugins/uploadKit.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\ETS Staff\Desktop\ALTSPACE\GITHUB\Project_7_simple\Proj_7_Kit_simple\Project_7_simple\1580305646052442528_wbr2_step_c.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1580305646052442528.json
+curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\ETS Staff\Desktop\ALTSPACE\GITHUB\Project_7_simple\Proj_7_Kit_simple\Project_7_simple\1580353092019290166_wbr2_kit_lesson.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1580353092019290166.json
