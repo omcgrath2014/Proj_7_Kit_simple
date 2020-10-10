cc3a70728d9e9734bb36d59de158efc21772359f
omcgrath2014 <>
2020-10-10 13:37:20 -0700

    fini version

--- Project_7_simple/Assets/Plugins/uploadKit.bat
+++ Project_7_simple/Assets/Plugins/uploadKit.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\ETS Staff\Desktop\ALTSPACE\GITHUB\Project_7_simple\Proj_7_Kit_simple\Project_7_simple\1580678618579730968_wbr2_proj_7.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1580678618579730968.json
+curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\ETS Staff\Desktop\ALTSPACE\GITHUB\Project_7_simple\Proj_7_Kit_simple\Project_7_simple\1580678618579730968_wbr2_final.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1580678618579730968.json
